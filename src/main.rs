b7f07ef840f2dc28be3a41db97b21b1ec8e570e5
4rgon4ut <egorlaptev98@gmail.com>
2024-01-29 14:56:11 +0300

    solution

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,15 @@ use ark_ec::{
     AffineRepr, CurveGroup,
 };
 use ark_ff::field_hashers::DefaultFieldHasher;
-
+use ark_ff::*;
 use ark_serialize::{CanonicalDeserialize, Read};
 
 use prompt::{puzzle, welcome};
 
 use sha2::Sha256;
-use std::fs::File;
 use std::io::Cursor;
 use std::ops::{Mul, Neg};
+use std::{fs::File, ops::Sub};
 
 use ark_std::{rand::SeedableRng, UniformRand, Zero};
 
@@ -76,13 +76,53 @@ fn main() {
         .for_each(|(i, (pk, proof))| pok_verify(*pk, i, *proof));
 
     let new_key_index = public_keys.len();
-    let message = b"YOUR GITHUB USERNAME";
+    let message = b"4rgon4ut";
 
     /* Enter solution here */
 
-    let new_key = G1Affine::zero();
-    let new_proof = G2Affine::zero();
-    let aggregate_signature = G2Affine::zero();
+    let new_secret: Fr = Fr::zero();
+
+    let honest_aggregated_pk = public_keys
+        .iter()
+        .fold(G1Projective::zero(), |acc, (pk, _)| acc + pk);
+
+    /*
+        As stated in rogue key attack description, we can use the fact that the aggregated public key
+        is a linear combination of the honest public keys to compute the new key that will negate all
+        honest keys contributions.
+        As sk = 0 => pk = O (point at infinity)
+        Thus we can compute honest keys aggregation inverse as follows:
+    */
+    let new_key = G1Affine::generator()
+        .mul(new_secret) // pk = g^sk
+        .sub(honest_aggregated_pk.clone()) // find point that negates all honest keys
+        .into_affine();
+
+    /*
+        Now we have pk which is an inverse of honest keys sum
+        => sk of our negating pk (new_key) should be an inverse on g^sk1+g^sk2+...+g^skn
+        As we have list of proofs for those honest sks, where every proof is:
+        proof_i = r^i+1^sk_i     (note that rng result is always the same becouse of seeding, we can denote it as 'r')
+        Than our proof for new_key should be:
+        new_proof = r^new_key_index^(sk1+sk2+...+skn)^-1 where (sk1+sk2+...+skn)^-1 is inverse of sum of all honest sks
+
+        To get those sum of honest sks we should clear proofs from index factors and sum them up:
+        sum_of_honest_sks = sum(proof_i^(-1 * i+1))
+
+        Than multiply those sum by new_key_index by new_key_index and negate it to get new_proof:
+    */
+    let new_proof = public_keys
+        .iter()
+        .enumerate()
+        .fold(G2Projective::zero(), |acc, (i, (_, proof))| {
+            let inverse_factor = Fr::from(i as u64 + 1).inverse().unwrap();
+            acc + proof.mul(inverse_factor) // clear idx factor
+        })
+        .mul(Fr::from(new_key_index as u64 + 1)) // multiply by new_key_index to follow proof generation
+        .neg()
+        .into_affine();
+
+    let aggregate_signature = bls_sign(new_secret, message);
 
     /* End of solution */
 
